e943c20d343d960a8e959b5996b35149c127bcc5
Celeste Layne <layne.celeste@gmail.com>
2017-09-11 20:59:02 -0400

    request data from api

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 console.log('The bot is starting');
 
+var request = require('request');
+
+// Use the Twit node package
 var Twit = require('twit');
+
+// Pull in all Twitter & Rijksmuseum account info
 var config = require('./config');
-// console.log(config)
+
+// Make a Twit object for connection to the API
 var T = new Twit(config);
 
-T.post('statuses/update', { status: 'hello this is a test form the rijksmuseum bot' }, function(err, data, response) {
-  console.log(data)
-});
+// Concat URL
+var artURL = 'https://www.rijksmuseum.nl/api/en/collection?key=';
+var api_key = 'A8AOseGS';
+var params = '&format=json&ps=10&imgonly=true';
+var URL = artURL + api_key + params;
+
+	request.get({
+		uri: URL
+	},
+	function getData(err, res, body){
+		var data = JSON.parse(body).artObjects;
+		console.log(data);
+	});
