732caa71da479f8e56dc653f5a6708f72d941d26
Celeste Layne <layne.celeste@gmail.com>
2017-09-11 23:26:03 -0400

    random image selection

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ console.log('The bot is starting');
 
 var request = require('request');
 
+// For reading large image files
+var fs = require('fs');
+
+var path = require('path');
+
 // Use the Twit node package
 var Twit = require('twit');
 
@@ -13,14 +18,24 @@ var T = new Twit(config);
 
 // Concat URL
 var artURL = 'https://www.rijksmuseum.nl/api/en/collection?key=';
-var api_key = 'A8AOseGS';
+var rm_api = config.api_key;
 var params = '&format=json&ps=10&imgonly=true';
-var URL = artURL + api_key + params;
+var URL = artURL + rm_api + params;
+
+Array.prototype.pick = function(){
+	return this[Math.floor(Math.random()*this.length)];
+};
 
 	request.get({
-		uri: URL
+		uri: URL,
+		encoding: 'binary'
 	},
 	function getData(err, res, body){
-		var data = JSON.parse(body).artObjects;
-		console.log(data);
+		var data = JSON.parse(body).artObjects.pick();
+		// console.log('Saving image to folder...')
+
+		var random_image = data.webImage.url;
+		console.log(random_image)
+		// fs.writeFile('downloaded.jpg', random_image, 'binary', function (err) {});
+
 	});
